ce39e63938c7705445b174d836147d3ba65084cc
Amitesh <asrivastava@dminc.com>
2018-04-09 13:44:03 +0530

    learning jenkins

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,9 +4,8 @@ public class HelloWorld2 {
 	
 	public static void main(String[] args) {
 		System.out.println("HelloWorld2!!");
-System.out.println("HelloWorld2!!");
-System.out.println("HelloWorld2!!");
-System.out.println("HelloWorld2!!”);
-System.out.println("HelloWorld2!!”);
+		for(int i=0;i<11;i++){
+			System.out.println("i="+i);
+		}
 	}
 }
